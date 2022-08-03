fcdce4fcf4e2d51f914889d2fb48ac97dc9edd5a
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-08-03 19:14:19 -0400

    Added events architecture

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const TOKEN = "MTAwNDQ5OTI1MDI3ODgzMDI4MA.Gv3FBx.ENqKum2ZTxPDF4GZCBRWqGLkLptgXD_IuBK23w";
-
 const client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
         Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.MessageContent
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers
     ]
-});
-
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
 })
 
-client.on("messageCreate", (message) => {
-    if(message.content.toLowerCase() == "hi") {
-        message.reply(`Hello ${message.author.username}!`);
-    }
-})
+let bot = {
+    client,
+    prefix: "q.",
+    owners: ["224217906643206145"]
+}
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+
+client.loadEvents(bot, false);
 
 client.login(process.env.TOKEN);
+
+module.exports = bot
