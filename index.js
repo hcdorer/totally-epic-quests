3f48c817c5bcda76a7b74bd67de934ab768ec733
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-08-04 03:07:34 -0400

    Added ping command using discord.js guide's tutorial

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
+client.on("interactionCreate", async interaction => {
+    if(!interaction.isChatInputCommand()) {
+        return;
+    }
+
+    const { commandName } = interaction
+
+    if(commandName === "ping") {
+        await interaction.reply("Pong!")
+    }
+})
+
 client.login(token)
