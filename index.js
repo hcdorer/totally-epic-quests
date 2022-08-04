840f30fc47142af4b20ed10ce69f5cc885d1a4f6
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-08-04 03:47:19 -0400

    Set up command handler system (again, per discord.js guides)

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const { Client, GatewayIntentBits } = require("discord.js")
-const { token } = require("./config.json")
+const { Client, Collection, GatewayIntentBits } = require(`discord.js`)
+const { token } = require(`./config.json`)
+const fs = require(`fs`)
+const path = require(`path`)
+const { dirname } = require("path")
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]})
+
+client.commands = new Collection()
+const commandsPath = path.join(__dirname, `commands`)
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(`.js`))
+
+for(const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+
+    client.commands.set(command.data.name, command)
+}
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -12,10 +26,16 @@ client.on("interactionCreate", async interaction => {
         return;
     }
 
-    const { commandName } = interaction
+    const command = client.commands.get(interaction.commandName)
+    if(!command) {
+        return
+    }
 
-    if(commandName === "ping") {
-        await interaction.reply("Pong!")
+    try {
+        await command.execute(interaction)
+    } catch(error) {
+        console.error(error)
+        await interaction.reply({content: `There was an error executing this command!`, ephemeral: true})
     }
 })
 
