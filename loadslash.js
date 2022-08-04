4d803880b2d86ddbb938a1957a73eb454ef1c9b9
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-08-03 21:55:42 -0400

    Set up timeout slash command (and slash command architecture)

--- loadslash.js
+++ loadslash.js
@@ -30,30 +30,7 @@ client.on("ready", async () => {
 
     await guild.commands.set([...client.slashcommands.values()])
     console.log(`Successfully loaded in ${client.slashcommands.size} slash command(s)`)
-})
-
-client.on("interactionCreate", (interaction) => {
-    if(!interaction.isCommand()) {
-        return
-    }
-
-    if(!interaction.inGuild()) {
-        return interaction.reply("This command can only be used in a server")
-    }
-
-    const slashCommand = client.slashcommands.get(interaction.commandName)
-
-    if(!slashCommand) {
-        return interaction.reply("Invalid slash command")
-    }
-
-    if(slashCommand.permission && !interaction.member.permissions.has(slashCommand.permission)) {
-        return interaction.reply("You do not have permission for this command")
-    }
-
-    slashCommand.run(client, interaction)
+    process.exit(0)
 })
 
 client.login(process.env.TOKEN)
-
-module.exports = bot
---
+++ slashcommands/timeout.js
@@ -0,0 +1,57 @@
+const { PermissionFlagsBits } = require("discord.js")
+
+const durations = [
+	{ name: "60 seconds", value: 60 * 1000 },
+	{ name: "5 minutes", value: 5 * 60 * 1000 },
+	{ name: "10 minutes", value: 10 * 60 * 1000 },
+	{ name: "30 minutes", value: 30 * 60 * 1000 },
+	{ name: "1 hour", value: 60 * 60 * 1000 },
+	{ name: "1 day", value: 24 * 60 * 60 * 1000 },
+	{ name: "1 week", value: 7 * 24 * 60 * 60 * 1000 },
+]
+
+const run = async(client, interaction) => {
+    let member = interaction.options.getMember("user")
+    let duration = interaction.options.getNumber("duration")
+    let reason = interaction.options.getString("reason") || "No reason given"
+
+    if(!member) {
+        return interaction.reply ("Invalid member")
+    }
+
+    try {
+        await member.timeout(duration, reason)
+        return interaction.reply(`${member.user.tag} has been timed out for ${durations.find(d => duration === d.value)?.name} for ${reason}`)
+    } catch(err) {
+        console.error(err)
+        return interaction.reply(`Failed to time out ${member.user.tag}`)
+    }
+}
+
+module.exports = {
+    name:"timeout",
+    description:"Time out a member",
+    permission: "MODERATE_MEMBERS",
+    options: [
+        {
+            name: "user",
+            description: "The user to time out",
+            type: 6,
+            required: true
+        },
+        {
+            name: "duration",
+            description: "The duration of the timeout",
+            type: 10,
+            choices: durations,
+            required: true
+        },
+        {
+            name: "reason",
+            description: "Reason for timing out this user",
+            type: 3,
+            required: false
+        }
+    ],
+    run
+}
