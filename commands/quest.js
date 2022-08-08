56aef4cbdf0a72e009be8c2146c083b5df9efcf6
Harry Dorer <henry.dorer@mymail.champlain.edu>
2022-08-07 23:52:46 -0400

    Created /quest delete command

--- commands/quest.js
+++ commands/quest.js
@@ -20,6 +20,13 @@ module.exports = {
             .addNumberOption(option => option
                 .setName(`reward`)
                 .setDescription(`How much experience a member recieves upon completing the quest`)
+                .setRequired(true)))
+        .addSubcommand(subcommand => subcommand
+            .setName(`delete`)
+            .setDescription(`Delete an existing quest`)
+            .addStringOption(option => option
+                .setName(`name`)
+                .setDescription(`The name of the quest to delete`)
                 .setRequired(true))),
     async execute(logger, interaction) {
         logger.log(`${interaction.user.tag} used /quest`)
@@ -39,6 +46,13 @@ module.exports = {
             let name = interaction.options.getString(`name`)
             let description = interaction.options.getString(`description`)
             let reward = interaction.options.getNumber(`reward`)
+            
+            if(quests[name]) {
+                logger.log(`A quest named ${name} already exists`)
+                logger.newline()
+
+                return interaction.reply({content: `That quest already exists!`, ephemeral: true})
+            }
 
             quests[name] = new Quest(description, reward)
             saveQuests(logger, interaction.guildId, quests)
@@ -47,6 +61,32 @@ module.exports = {
             logger.newline()
 
             interaction.reply({content: `Quest created!`, ephemeral: true})
+        }
+        if(interaction.options.getSubcommand() === `delete`) {
+            logger.log(`Subcommand: delete`)
+
+            if(!interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
+                logger.log(`${interaction.user.tag} had insufficient permissions`)
+                logger.newline()
+                
+                return interaction.reply({content: `You do not have permission to do this!`, ephemeral: true})
+            }
+
+            let name = interaction.options.getString(`name`)
+
+            if(!quests[name]) {
+                logger.log(`No quest named ${name} exists`)
+                logger.newline()
+
+                return interaction.reply({content: `There's no quest named ${name}!`, ephemeral: true})
+            }
+
+            delete quests[name]
+
+            logger.log(`Deleted the ${name} quest`)
+            logger.newline()
+
+            interaction.reply({content: `Quest deleted!`, ephemeral: true})
         }
     }
 }
