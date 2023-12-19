b6023f20042d04dea489840fcc80952f9a265f5b
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-12-19 17:23:57 -0500

    Created functions for quest subcommand functionality

--- utils/util-functions.js
+++ utils/util-functions.js
@@ -14,9 +14,11 @@ function permissionCheck(logger, interaction, permission, onPass, onFail = null)
     if(interaction.memberPermissions.has(permission)) {
         onPass()
     } else {
-        if(!onfail) {
+        if(!onFail) {
             logger.log(`${interaction.user.tag} does not have permission to do this`)
             interaction.reply("You do not have permission to do this!")
+        } else {
+            onFail()
         }
     }
 }
