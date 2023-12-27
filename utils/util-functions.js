ff074b00dc526271b44e59c092647e509f7968e3
hdorer <41219697+hdorer@users.noreply.github.com>
2023-12-26 22:12:26 -0500

    Some missing commits from Version 1.2 (#34)
    * The bot can now send patch notes to the message channel of every guild that it is in (closes #31)
    
    * Added version 1.2 patch notes
    
    ---------
    
    Co-authored-by: hcdorer <41219697+hcdorer@users.noreply.github.com>

--- utils/util-functions.js
+++ utils/util-functions.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require(`path`);
+
 /**
  * @param {Object} source 
  * @param {Object} target 
@@ -23,4 +26,29 @@ function permissionCheck(logger, interaction, permission, onPass, onFail = null)
     }
 }
 
-module.exports = { convertObject, permissionCheck }
+/**
+ * @param {Logger} logger 
+ * @param {string} patchNoteFile
+ * @returns {Promise<string>}
+ */
+function loadPatchNotes(logger, patchNoteFile) {
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((resolve, reject) => {
+        const patchNotePath = path.join(__dirname, `patch-notes`, patchNoteFile);
+        let patchNotesText = ``;
+
+        logger.log(`Reading patch note file located at ${patchNotePath}`);
+        fs.readFile(patchNotePath, `utf8`, (error, data) => {
+            if(error) {
+                throw(error);
+            }
+            
+            patchNotesText = data;
+            console.log(patchNotesText);
+
+            resolve(patchNotesText);
+        });
+    });
+}
+
+module.exports = { convertObject, permissionCheck, loadPatchNotes, }
